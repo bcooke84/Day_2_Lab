f3f9c3077d52d6331df57dd01dccc076a529c39a
Paul Kelly <paul.kelly20@gmail.com>
2018-03-27 15:55:12 +0100

    Short months method added

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -19,46 +19,50 @@ def divide(num_1, num_2)
 end
 
 def length_of_string(string)
-   return string.length
- end
+  return string.length
+end
 
- def join_string(string_1, string_2)
-   return string_1 + string_2
- end
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
 
- def add_string_as_number(string_1, string_2)
-   return string_1.to_i + string_2.to_i
- end
+def add_string_as_number(string_1, string_2)
+  return string_1.to_i + string_2.to_i
+end
 
- def number_to_full_month_name(month)
- case month
- when 1
-   "January"
- when 2
-   "Feburary"
- when 3
-   "March"
- when 4
-   "April"
- when 5
-   "May"
- when 6
-   "June"
- when 7
-   "July"
- when 8
-   "August"
- when 9
-   "September"
- when 10
-   "October"
- when 11
-   "November"
- when 12
-   "December"
- end
-
-
+def number_to_full_month_name(month)
+  case month
+  when 1
+    "January"
+  when 2
+    "Feburary"
+  when 3
+    "March"
+  when 4
+    "April"
+  when 5
+    "May"
+  when 6
+    "June"
+  when 7
+    "July"
+  when 8
+    "August"
+  when 9
+    "September"
+  when 10
+    "October"
+  when 11
+    "November"
+  when 12
+    "December"
+  end
 
 
- end
+end
+
+def number_to_short_month_name(month)
+  short_month = number_to_full_month_name(month).slice(0,3)
+
+  return short_month
+end
